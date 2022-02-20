60d0f196f93133f2702454f41aa63c6811e07317
Braquetes <braquetesss@gmail.com>
2022-02-20 16:15:40 -0600

    bd-update

--- db/sitiowebut.sql
+++ db/sitiowebut.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-02-2022 a las 03:45:37
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
+-- Tiempo de generación: 20-02-2022 a las 23:14:52
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,10 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `sitiowebut`
 --
-
-CREATE DATABASE `sitiowebut`;
-
-USE `sitiowebut`;
 
 -- --------------------------------------------------------
 
@@ -55,6 +51,29 @@ CREATE TABLE `productos` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id_usuarios` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `apellido_paterno` varchar(15) NOT NULL,
+  `apellido_materno` varchar(15) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `usuario` varchar(15) NOT NULL,
+  `pass` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id_usuarios`, `nombre`, `apellido_paterno`, `apellido_materno`, `correo`, `usuario`, `pass`) VALUES
+(1, 'Mara Andrea', 'Martinez ', 'Carreño', 'and.guerrero.n@gmail.com', 'Drea', 'RB*137946');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -65,6 +84,13 @@ CREATE TABLE `usuarios` (
   `tipo` tinyint(2) NOT NULL,
   `actualizacion` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_user`, `user`, `password`, `tipo`, `actualizacion`) VALUES
+(1, 'braquetesss@gmail.com', 'RB*137946', 1, '2022-02-20 07:36:30');
 
 --
 -- Índices para tablas volcadas
@@ -83,6 +109,12 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`),
   ADD KEY `id_categoria` (`id_categoria`),
   ADD KEY `id_instructor` (`id_instructor`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_usuarios`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -107,21 +139,16 @@ ALTER TABLE `productos`
   MODIFY `id_producto` int(10) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`),
-  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`id_instructor`) REFERENCES `instructores` (`id_instructor`);
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
